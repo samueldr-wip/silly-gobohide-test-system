c104a37908bc311dde42fea6fd71284a8828e8c3
Samuel Dionne-Riel <samuel@dionne-riel.com>
2026-09-07 11:38:06 -0400

    kernel: Read the repository for version info

--- configuration/kernel.nix
+++ configuration/kernel.nix
@@ -4,9 +4,39 @@
   wip.kernel.package = pkgs.callPackage (
     { stdenv }:
 
-    stdenv.mkDerivation {
-      version = "7.1.0";
+    stdenv.mkDerivation (finalAttrs: {
+      version =
+        let
+          # Keep the version information "evergreen"
+          makefile = builtins.readFile (finalAttrs.src + "/Makefile");
+          data =
+            builtins.filter
+            (s: builtins.isString s && (builtins.match "^(VERSION|PATCHLEVEL|SUBLEVEL|EXTRAVERSION) =.*$" s) != null)
+            (builtins.split "\n" makefile)
+          ;
+          info =
+            builtins.listToAttrs (
+              builtins.map (
+                s:
+                let
+                  getPart = builtins.elemAt (builtins.match "([^ ]*) *= *(.*)" s);
+                in
+                { name = getPart 0; value = getPart 1; }
+              )
+              data
+            )
+          ;
+        in
+          builtins.concatStringsSep "" [
+            info.VERSION
+            "."
+            info.PATCHLEVEL
+            "."
+            info.SUBLEVEL
+            info.EXTRAVERSION
+          ]
+      ;
       src = builtins.fetchGit /Users/samuel/tmp/linux/gobohide;
-    }
+    })
   ) {};
 }
